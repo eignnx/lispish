b698fb64b2db4a45da4cdfab4c7cf605f47d5726
Aditya Khandkar <adiya.s.khandkar@gmail.com>
2018-08-06 15:12:11 -0400

    eval() code added in trees class.

--- trees.py
+++ trees.py
@@ -1,6 +1,8 @@
+from proc import Proc
+
 class Expression:
-    def eval(self):
-        raise NotImplementedError()
+    def eval(self, env):
+        
     
 
 class Atom(Expression):
@@ -16,12 +18,13 @@ class Atom(Expression):
 
 
 class Number(Atom):
-    def eval(self):
+    def eval(self, env):
         return self
 
 
 class Symbol(Atom):
-    pass
+    def eval(self, env):
+        return env[self.val] # val is a str (the symbol's name).
 
 
 class List(Expression):
@@ -35,4 +38,33 @@ class List(Expression):
     def __repr__(self):
         vals = ", ".join(repr(v) for v in self.values)
         return "List({})".format(vals)
+
+    def eval(self, env) -> Expression:
+        first_arg = self.values[0]
+        rest_args = self.values[1:]
+        if type(first_arg) is Proc:
+            proc = first_arg
+            args = rest_args
+            # apply takes a list comprehension 
+            # with every value in args evaluated.
+            return proc.apply([a.eval(env) for a in args])
+        elif type(first_arg) is Symbol:
+            if first_arg.val == "define":
+                if type(rest_args[0]) is Symbol:
+                    env.declare(rest_args[0].val)
+                    retVal = rest_args[1].eval(env) 
+                    env[rest_args[0].val] = retVal
+                else: raise Error("Define only accepts Symbol")
+            elif first_arg.val == "lambda":
+                raise NotImplementedError("Define lambda!")
+            else: # it must be a user-defined symbol
+                proc = first_arg.eval(env)
+                
+                if type(proc) is not Proc:
+                    raise Error("Initial list element must be a Proc!")
+
+                new_self = List([proc] + rest_args)
+                return new_self.eval(env)
+
+        return None
         
