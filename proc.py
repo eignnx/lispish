965365e15aed868f347fc4056f1b7d43cb3272e5
eignnx <eignnx@gmail.com>
2018-08-07 15:11:01 -0400

    Annotate types of Proc constructor args

--- proc.py
+++ proc.py
@@ -1,8 +1,8 @@
-from trees import Expression
 from env import Env
+from typing import Collection
 
-class Proc(Expression):
-    def __init__(self, formals, body, creation_env):
+class Proc:
+    def __init__(self, formals: Collection[str], body, creation_env: Env):
         self.formals = formals # The names of the formal parameters
         self.body = body # The body AST
         self.creation_env = creation_env # A ref to the env where the proc was defined
